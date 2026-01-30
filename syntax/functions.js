68bf8eedc26d7562a8b30210e35c486efb094583
Gyimesi Gergo Mark <gyimesi.gergo@BOLYAI.LOCAL>
2026-01-30 10:01:08 +0100

    órai munka

---
+++ syntax/functions.js
@@ -0,0 +1,17 @@
+const muvelet = (a, b, callback) =>
+{
+    const result = callback(a,b)
+    return {
+        result
+    }
+}
+
+const muveletLetrehoz = (jel) =>
+{
+    if (jel == "+")
+    {
+        return (a,b) => { return a+b }
+    }
+}
+
+export {muvelet, muveletLetrehoz}
---
+++ syntax/index.js
@@ -0,0 +1,26 @@
+import { muvelet, muveletLetrehoz } from "./functions.js"
+
+
+const input = document.createElement('input')
+document.body.appendChild(input)
+const inputKetto = document.createElement('input')
+document.body.appendChild(inputKetto)
+const div = document.createElement('div')
+document.body.appendChild(div)
+const gomb = document.createElement('button')
+gomb.innerText = 'Valami'
+document.body.appendChild(gomb)
+
+gomb.addEventListener('click', function(){
+    const osszeg = Number(input.value)
+    const osszegKetto = Number(inputKetto.value)
+
+    //const eredmeny = muvelet(osszeg,osszegKetto, muveletLetrehoz("+") )
+
+    const {result} = muvelet(osszeg,osszegKetto, muveletLetrehoz("+") )
+
+    div.innerText = result
+})
+
+const fv = muveletLetrehoz("+")
+console.log(fv(1,2))
